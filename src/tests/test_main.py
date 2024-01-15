64cd93806cbe7f49585af78c829d9b710fd05e10
jewon119 <jewon119@gmail.com>
2024-01-15 12:26:32 +0900

    add: TestClient를 fixture로 만들어 재사용

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,23 +1,21 @@
-from fastapi.testclient import TestClient
-
 from database.orm import ToDo
-from main import app
-
-client = TestClient(app=app)
 
 
-def test_health_check():
+def test_health_check(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
 
 
-def test_get_todos(mocker):
+def test_get_todos(client, mocker):
     # order=ASE
-    mocker.patch("main.repository.get_todos", return_value=[
-        ToDo(id=1, contents="content 1", is_done=True),
-        ToDo(id=2, contents="content 2", is_done=False),
-    ])
+    mocker.patch(
+        "main.repository.get_todos",
+        return_value=[
+            ToDo(id=1, contents="content 1", is_done=True),
+            ToDo(id=2, contents="content 2", is_done=False),
+        ],
+    )
     response = client.get("/todos")
     assert response.status_code == 200
     assert response.json() == {
