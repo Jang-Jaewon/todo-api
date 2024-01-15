0be4f54911950c9698a6e604323a30ae5107d63a
jewon119 <jewon119@gmail.com>
2024-01-15 12:21:35 +0900

    add: pytest-mock 패키지 설치 및 mocker 활용

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+from database.orm import ToDo
 from main import app
 
 client = TestClient(app=app)
@@ -11,18 +12,18 @@ def test_health_check():
     assert response.json() == {"ping": "pong"}
 
 
-def test_get_todos():
+def test_get_todos(mocker):
     # order=ASE
+    mocker.patch("main.repository.get_todos", return_value=[
+        ToDo(id=1, contents="content 1", is_done=True),
+        ToDo(id=2, contents="content 2", is_done=False),
+    ])
     response = client.get("/todos")
     assert response.status_code == 200
     assert response.json() == {
         "todos": [
             {"id": 1, "contents": "content 1", "is_done": True},
             {"id": 2, "contents": "content 2", "is_done": False},
-            {"id": 4, "contents": "content 4", "is_done": False},
-            {"id": 5, "contents": "content 5", "is_done": False},
-            {"id": 6, "contents": "content 6", "is_done": False},
-            {"id": 7, "contents": "content 7", "is_done": True},
         ]
     }
 
@@ -31,10 +32,6 @@ def test_get_todos():
     assert response.status_code == 200
     assert response.json() == {
         "todos": [
-            {"id": 7, "contents": "content 7", "is_done": True},
-            {"id": 6, "contents": "content 6", "is_done": False},
-            {"id": 5, "contents": "content 5", "is_done": False},
-            {"id": 4, "contents": "content 4", "is_done": False},
             {"id": 2, "contents": "content 2", "is_done": False},
             {"id": 1, "contents": "content 1", "is_done": True},
         ]
