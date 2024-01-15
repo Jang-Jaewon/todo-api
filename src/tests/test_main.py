8b9a1997b2d29fe404b1026dc02741a8a6a5091b
jewon119 <jewon119@gmail.com>
2024-01-15 11:53:55 +0900

    add: todo 전체 조회 api 테스트 코드 작성(mocking x)

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -9,3 +9,33 @@ def test_health_check():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
+
+
+def test_get_todos():
+    # order=ASE
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 1, "contents": "content 1", "is_done": True},
+            {"id": 2, "contents": "content 2", "is_done": False},
+            {"id": 4, "contents": "content 4", "is_done": False},
+            {"id": 5, "contents": "content 5", "is_done": False},
+            {"id": 6, "contents": "content 6", "is_done": False},
+            {"id": 7, "contents": "content 7", "is_done": True},
+        ]
+    }
+
+    # order=DESC
+    response = client.get("/todos?order=DESC")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 7, "contents": "content 7", "is_done": True},
+            {"id": 6, "contents": "content 6", "is_done": False},
+            {"id": 5, "contents": "content 5", "is_done": False},
+            {"id": 4, "contents": "content 4", "is_done": False},
+            {"id": 2, "contents": "content 2", "is_done": False},
+            {"id": 1, "contents": "content 1", "is_done": True},
+        ]
+    }
