02ae1dc4b9fffdc0a6cf704cf71f3ad0ed5887c2
jewon119 <jewon119@gmail.com>
2024-01-31 16:13:12 +0900

    add: 회원가입 테스트 코드 구현

--- src/tests/test_users_api.py
+++ src/tests/test_users_api.py
@@ -1,4 +1,36 @@
-def test_sign_up_user(client):
-    response = client.post("/users/sign-up")
+from database.orm import User
+from database.repository import UserRepository
+from service.user import UserService
+
+
+def test_sign_up_user(client, mocker):
+    hash_password = mocker.patch.object(
+        UserService, "hash_password", return_value="hashed"
+    )
+    user_create = mocker.patch.object(
+        User,
+        "create",
+        return_value=User(
+            id=None,
+            username="test",
+            password="hashed",
+        ),
+    )
+    mocker.patch.object(
+        UserRepository,
+        "save_user",
+        return_value=User(
+            id=1,
+            username="test",
+            password="hashed",
+        ),
+    )
+
+    body = {"username": "test", "password": "plain"}
+    response = client.post("/users/sign-up", json=body)
+
+    hash_password.assert_called_once_with(plain_password="plain")
+    user_create.assert_called_once_with(username="test", hashed_password="hashed")
+
     assert response.status_code == 201
-    assert response.json() is True
+    assert response.json() == {"id": 1, "username": "test"}
